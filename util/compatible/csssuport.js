14fe06395be78174c93bb5b3ec66c99a149aa3d7
zhangmingrui <592044573@qq.com>
2016-11-15 17:09:47 +0800

    libcompatible/csssuport 组件新增是否支持transitions属性判断

--- util/compatible/csssuport.js
+++ util/compatible/csssuport.js
@@ -2,13 +2,18 @@
  * @fileoverview css支持情况判断。主要用于浏览器兼容
  * @version 1.0 | 2015-08-31 版本信息
  * @author Zhang Mingrui | 592044573@qq.com
- * @return 
+ * @return
  * @example
  * requirejs(['libcompatible/csssuport'],function($csssuport){
  * 	 $csssuport.fixed;
  * });
  * */
-define({
-	//是否支持position:fixed定位
-	fixed: true
+define(function(){
+	var _div = document.createElement('div');
+	return {
+		//是否支持position:fixed定位
+		fixed: !('undefined' == typeof(document.body.style.maxHeight) || (document.compatMode !== "CSS1Compat" && /msie/.test(navigator.userAgent.toLowerCase()))),
+		//是否支持transition
+		transition: !(_div.style.transition == undefined)
+	};
 });
